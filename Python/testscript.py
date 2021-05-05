d3a3c04b0686c2fd46b0eac7d2b816ab68ec2852
Logan Ross <logo3102@gmail.com>
2021-05-05 15:38:17 -0700

    Update testscript.py

--- Python/testscript.py
+++ Python/testscript.py
@@ -1,6 +1,8 @@
 import serial
 
 port = serial.Serial('COM3', baudrate=9600)
+port.read_timeout = 1
 
 port.write('SAAF'.encode())
-print(port.read())
+response = port.read_until('X'.encode())
+print(response)
