8714aa8270641af654aaf78e3703156459297465
TakingFire <bacurrah@gmail.com>
2021-05-06 23:51:18 -0700

    More gui options

--- Python/PythonCommandCenter.py
+++ Python/PythonCommandCenter.py
@@ -16,8 +16,14 @@ print(f'Port opened at {port.name}')
 # Window Layout #
 
 leftcol = [
-           [sg.Radio('1', 1, True), sg.Radio('5', 1), sg.Radio('10', 1)],
-           [sg.Listbox(values=['Placeholder', 'More Placeholder', 'etc.'], no_scrollbar=True, s=(18,9), enable_events=True, k='Preset')]
+           [sg.Text('Step Size'), sg.Spin([i for i in range(1,360)], initial_value=15, size=(6,1))],
+           [sg.Text('Step Speed'), sg.Spin([i for i in range(1,500)], initial_value=250, size=(5,1))],
+           [sg.Listbox(values=['Placeholder', 'More Placeholder', 'etc.'], no_scrollbar=True, s=(19,8), enable_events=True, k='Preset')]
+]
+
+rightcol = [
+            [sg.Radio('Step', 2, True), sg.Radio('Servo', 2)],
+            [sg.Output(s=(14,9), echo_stdout_stderr=True)]
 ]
 
 dirbuttons = [
@@ -31,18 +37,20 @@ dirbuttons = [
 layout = [
           [sg.Column(leftcol, vertical_alignment='bottom'),
            sg.Column(dirbuttons, element_justification='center'),
-           sg.Column([[sg.Output(s=(10,10), echo_stdout_stderr=True)]])]
+           sg.Column(rightcol)]
 ]
 
-window = sg.Window('BMC Controller', layout)
+window = sg.Window('BMC Controller', layout, font=('Consolas', 10))
 
 # Main Loop #
 
-def motorMove(pos, dir, time, dist):
+def motorMove(pos, dir, time, step):
     try:
-        port.write((':'+':'.join([pos, dir, time, dist])+':').encode())
-        # port.read_until('X'.encode())
-        sleep(int(time)/1000)
+        command = (':'+':'.join([pos, dir, time, step])+':')
+        print(command)
+        port.write(command.encode())
+        sleep(int(time)/1000)  # port.read_until('X'.encode())
+
     except serial.serialutil.SerialTimeoutException: print('Timed out!')
 
 while True:
@@ -53,7 +61,9 @@ while True:
         break
 
     if event != '': print(event)
-    step = 'A' if values[0] else 'B' if values[1] else 'C'
+    pos = 'A' if values[2] else 'G'
+    time = str(values[1])
+    step = str(values[0])
 
-    if event == 'F': motorMove('A', 'F', '500', '10')
-    if event == 'B': motorMove('A', 'B', '500', '10')
+    if event == 'F': motorMove(pos, 'F', time, step)
+    if event == 'B': motorMove(pos, 'B', time, step)
