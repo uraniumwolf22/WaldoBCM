bcac7b58d0a19fd252d1d2029392bcc8dfb8a957
uraniumwolf22 <36753028+uraniumwolf22@users.noreply.github.com>
2021-06-10 00:59:36 -0700

    Changed some crap n stuff

--- ServerTests/Client.py
+++ ServerTests/Client.py
@@ -1,8 +1,8 @@
+import socket
+
 HOST = '10.1.10.52'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
+s.sendall('Hello, world'.encode())
 s.close()
-print('Received', repr(data))
