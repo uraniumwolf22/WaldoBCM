466085b88166ab0e9ecff49e20c3bb19acaf2d32
uraniumwolf22 <36753028+uraniumwolf22@users.noreply.github.com>
2021-06-10 17:38:22 -0700

    Braden dont kill me

--- ServerTests/Server.py
+++ ServerTests/Server.py
@@ -25,31 +25,18 @@ def update(deg, dist, line):
     return line,
 
 
+while 1:
+    data = conn.recv(16384)
+    if not data: break
+    # conn.sendto('success'.encode(), addr)
+    try: scan = json.loads(data.decode())
+    except json.decoder.JSONDecodeError: continue
 
-def get_lidar_data():
-    HOST = '0.0.0.0'         # Symbolic name meaning all available interfaces
-    PORT = 8000              # Arbitrary non-privileged port
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    print('Connected by', addr)
-
-    while 1:
-        data = conn.recv(16384)
-        if not data: break
-        # conn.sendto('success'.encode(), addr)
-        try: scan = json.loads(data.decode())
-        except json.decoder.JSONDecodeError: continue
-
-        deg = []
-        dist = []
-        for i in scan:
-            deg.append(math.radians(i[1]))
-            dist.append(i[2])
-        line, = update(deg, dist, line)
-        #print(deg, dist)
+    deg = []
+    dist = []
+    for i in scan:
+        deg.append(math.radians(i[1]))
+        dist.append(i[2])
+    line, = update(deg, dist, line)
+    #print(deg, dist)
     # print(data.decode())
-
-LidarData = threading.Thread(target=get_lidar_data, daemon=True)
-LidarData.start()
