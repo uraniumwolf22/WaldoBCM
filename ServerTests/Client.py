5a1fe5a1ee425fa098ffbc74582a088c0e93788d
TakingFire <bacurrah@gmail.com>
2021-06-11 17:08:03 -0700

    Reorder client connections

--- ServerTests/Client.py
+++ ServerTests/Client.py
@@ -2,14 +2,14 @@ import socket
 from rplidar import RPLidar
 import json
 
+liport = RPLidar('COM7')
+print(liport.get_info())
+print(liport.get_health())
+
 HOST = 'rossnation.ddns.net'    # The remote host
 PORT = 8000            # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-
-liport = RPLidar('COM7')
-print(liport.get_info())
-print(liport.get_health())
 
 for scan in liport.iter_scans(scan_type='express'):
     # print(scan)
