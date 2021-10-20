f57a6f07ac50a3bb796ce7fc34300051d5c931cf
Logan Ross <logo3102@gmail.com>
2021-10-19 18:42:47 -0700

    Added faster XY conversion

--- ServerTests/Host.py
+++ ServerTests/Host.py
@@ -12,7 +12,9 @@ s.listen(1)
 conn, addr = s.accept()
 print('Connected by', addr)
 
-while 1:
+
+repeat = True
+while repeat:
     x = []
     y = []
     print("SCAN RESET")
@@ -28,51 +30,12 @@ while 1:
     for i in scan:
         deg.append((math.radians(i[1])) * 180 / math.pi )
         dist.append((i[2]))
-    # print(deg, dist)
-    # print("deg = ",deg)
+    print(deg, dist)
+
 
     for pair in range(len(deg)):
-        if deg[pair] == 0:
-            x.append(0)
-            y.append(dist[pair])
-
-        elif deg[pair] == 90:
-            x.append(dist(pair))
-            y.append(0)
-
-        elif deg[pair] == 180:
-            x.append(0)
-            y.append(0 - dist[pair])
-
-        elif deg[pair] == 270:
-            x.append(0 - dist[pair])
-            y.append(0)
-
-
-        elif deg[pair] > 0 and deg[pair] < 90:
-            x.append(dist[pair] * math.cos(deg[pair]))
-            y.append(dist[pair] * math.sin(deg[pair]))
-            print("x = ", x[pair])
-            print("y = ", y[pair])
-
-
-        elif deg[pair] > 90 and deg[pair] < 180:
-            x.append(dist[pair] * (math.sin(deg[pair] - 90)))
-            y.append(0 - (dist[pair] * (math.cos(deg[pair] - 90))))
-            print("x = ", x[pair])
-            print("y = ", y[pair])            
-
-
-        elif deg[pair] > 180 and deg[pair] < 270:
-            x.append(0 - (dist[pair] * math.cos(deg[pair])))
-            y.append(0 - (dist[pair] * math.sin(deg[pair])))
-            print("x = ", x[pair])
-            print("y = ", y[pair])
-
-        elif deg[pair] > 270 and deg[pair] < 360:
-            x.append(0 - (dist[pair] * (math.sin(deg[pair] - 90))))
-            y.append(dist[pair] * (math.cos(deg[pair] - 90)))
-            print("x = ", x[pair])
-            print("y = ", y[pair])   
-
+        x.append(dist[pair] * math.cos(deg[pair]))
+        y.append(dist[pair] * math.sin(deg[pair]))
+        print("X = ", x[pair])
+        print("Y = ", y[pair])
 conn.close()
