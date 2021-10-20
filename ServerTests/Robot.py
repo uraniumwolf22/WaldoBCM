8f472755818ed0837b3690bbc32357148e2f564f
TakingFire <bacurrah@gmail.com>
2021-10-19 21:37:01 -0700

    Migrate to websockets

--- ServerTests/Robot.py
+++ ServerTests/Robot.py
@@ -1,20 +1,18 @@
-import socket
 from rplidar import RPLidar
+import websockets
+import asyncio
 import json
 
 liport = RPLidar('COM5')
 print(liport.get_info())
 print(liport.get_health())
 
-HOST = 'localhost'    # The remote host
-PORT = 8000            # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+async def main():
+    async with websockets.connect('ws://rossnation.ddns.me:8000') as ws:
+        for scan in liport.iter_scans(scan_type='express'):
+            # print(scan)
+            ws.send(json.dumps(scan).encode())
+            # response = s.recv(1024)
+            # print(response.decode())
 
-for scan in liport.iter_scans(scan_type='express'):
-    # print(scan)
-    s.sendall(json.dumps(scan).encode())
-    # response = s.recv(1024)
-    # print(response.decode())
-
-s.close()
+asyncio.run(main())
