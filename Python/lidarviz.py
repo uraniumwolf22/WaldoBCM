8c919cb62c10e01cf345d615cfa9d28127dccd46
TakingFire <bacurrah@gmail.com>
2021-10-19 18:16:20 -0700

    Updated visualization

--- Python/lidarviz.py
+++ Python/lidarviz.py
@@ -3,6 +3,10 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+port = RPLidar('COM7')
+print(port.get_info())
+print(port.get_health())
+
 plt.style.use('ggplot')
 plt.ion()
 
@@ -22,16 +26,31 @@ def update(deg, dist, line):
     plt.pause(0.001)
     return line,
 
-port = RPLidar('COM7')
-
-print(port.get_info())
-print(port.get_health())
+predeg = []
+predist = []
 
 for scan in port.iter_scans(scan_type='express'):
-    deg = []
-    dist = []
+    deg, dist, avgdeg, avgdist = [], [], [], []
+
+    # if predeg == []:
     for i in scan:
         deg.append(math.radians(i[1]))
+        predeg.append(math.radians(i[1]))
         dist.append(i[2])
+        predist.append(i[2])
     line, = update(deg, dist, line)
-    print(deg, dist)
+
+    # Attempt at averaging w/ previous scan
+
+    # else:
+    #     for i in scan:
+    #         deg.append(math.radians(i[1]))
+    #         dist.append(i[2])
+    #         for i in range(0, len(deg)):
+    #             try: avgdeg.append((predeg[i] + deg[i])/2)
+    #             except IndexError: avgdeg.append(deg[i])
+    #             try: avgdist.append((predist[i] + dist[i])/2)
+    #             except IndexError: avgdist.append(dist[i])
+    #     line, = update(avgdeg, avgdist, line)
+
+    # print(deg, dist)
