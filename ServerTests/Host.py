4d645be017cb2ec7988bce6eb6bd7f8e40a09181
TakingFire <bacurrah@gmail.com>
2021-10-19 20:29:12 -0700

    Socket hub v1

--- ServerTests/Host.py
+++ ServerTests/Host.py
@@ -1,41 +1,48 @@
-import socket
-import matplotlib.pyplot as plt
+import websockets
+import asyncio
 import math
 import json
-import base64
 
-HOST = '0.0.0.0'         # Symbolic name meaning all available interfaces
-PORT = 8000              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
+async def recv(ws, path):
+    while True:
+        print("SCAN RESET")
+        data = await ws.recv()
+        if not data: break
+        try: scan = json.loads(data.decode())
+        except json.decoder.JSONDecodeError: continue
+
+        deg = []
+        dist = []
+        for i in scan:
+            deg.append((math.radians(i[1])) * 180 / math.pi)
+            dist.append((i[2]))
+        print(deg, dist)
+
+        global packet
+        packet = json.dumps([deg,dist])
+
+        x = []
+        y = []
+        for pair in range(len(deg)):
+            x.append(dist[pair] * math.cos(deg[pair]))
+            y.append(dist[pair] * math.sin(deg[pair]))
+            print("X = ", x[pair])
+            print("Y = ", y[pair])
+
+async def send(ws, path):
+    message = await ws.recv()
+    print(json.loads(message))
+
+    while True:
+        global packet
+        await ws.send(packet)
+        await asyncio.sleep(0)
 
 
-repeat = True
-while repeat:
-    x = []
-    y = []
-    print("SCAN RESET")
-    data = conn.recv(16384)
-    if not data: break
-    try: scan = json.loads(data.decode())
-    except json.decoder.JSONDecodeError: continue
+if __name__ == "__main__":
+    server_bot = websockets.serve(recv, "0.0.0.0", 8000)
+    server_web = websockets.serve(send, "0.0.0.0", 8001)
 
-    deg = []
-    dist = []
-
-
-    for i in scan:
-        deg.append((math.radians(i[1])) * 180 / math.pi )
-        dist.append((i[2]))
-    print(deg, dist)
-
-
-    for pair in range(len(deg)):
-        x.append(dist[pair] * math.cos(deg[pair]))
-        y.append(dist[pair] * math.sin(deg[pair]))
-        print("X = ", x[pair])
-        print("Y = ", y[pair])
-conn.close()
+    asyncio.get_event_loop().run_until_complete(server_bot)
+    asyncio.get_event_loop().run_until_complete(server_web)
+    asyncio.get_event_loop().run_forever()
